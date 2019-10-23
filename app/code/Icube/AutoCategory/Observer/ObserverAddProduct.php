f1525aa85c63ca786cc6e0cf107c9add6977a1e2
FKOGNW <fiko.gunawan@icube.us>
2019-10-23 04:04:29 +0000

    Adding file for observer event save product

---
+++ app/code/Icube/AutoCategory/Observer/ObserverAddProduct.php
@@ -0,0 +1,14 @@
+<?php
+namespace Icube\AutoCategory\Observer;
+
+class ObserverAddProduct implements \Magento\Framework\Event\ObserverInterface
+{
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/product.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info('Your text message');
+    }
+}
