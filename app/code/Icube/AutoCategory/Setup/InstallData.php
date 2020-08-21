373edb86390a7ce3548b0e0849a8e558eecbd4aa
Lukman Suprayitno <lukman.suprayitno@sirclo.com>
2020-08-21 08:07:44 +0000

    rename Setup/Add.. to InstallData.php

--- app/code/Icube/AutoCategory/Setup/InstallData.php
+++ app/code/Icube/AutoCategory/Setup/InstallData.php
@@ -8,7 +8,7 @@ use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 
-class AddAttributeSet implements InstallDataInterface {
+class IntallData implements InstallDataInterface {
 
  private $eavSetupFactory;
 
