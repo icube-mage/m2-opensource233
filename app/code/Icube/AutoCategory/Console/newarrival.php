d2200c0320e1cff7969170fd56e6be0fe8554bfe
root <root@mgt-development-environment-72>
2020-08-24 08:25:27 +0000

    Added New Arrival Logic to add product by range days

--- app/code/Icube/AutoCategory/Console/newarrival.php
+++ app/code/Icube/AutoCategory/Console/newarrival.php
@@ -30,23 +30,48 @@ class newarrival extends Command
         $CategoryLinkRepository = $objectManager->get('\Magento\Catalog\Model\CategoryLinkRepository');
         $categoryLinkRepository = $objectManager->get('\Magento\Catalog\Api\CategoryLinkManagementInterface'); 
         $categoryID = 41;
-        $configValue = $this->helper->getConfigValue();
+        $configEnabled = $this->helper->getConfigEnabled();
+        $configRange = (string)$this->helper->getConfigRange();
         $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
         $collection = $productCollection->addAttributeToSelect('*')->load();
-        
-        if($configValue==1){
+        $currentDate = date('Y-m-d');
+        if($configEnabled==1){
             foreach ($collection as $product){
+                $startDate= date("Y-m-d",strtotime($product->getCreatedAt())); 
+                $endDate= date("Y-m-d",strtotime($product->getCreatedAt()."+ ".$configRange." day")); 
                 $sku = $product->getSku();
                 if($product->getExcludeFromNew()==0){
-                    $categoryIds= array('41');
-                    $categoryLinkRepository->assignProductToCategories($sku, $categoryIds);
-                    var_dump($product->getName());
-                    die();
-                }elseif($product->getExcludeFromNew()==1){
+                    if($startDate <= $currentDate && $endDate >= $currentDate){
+                        $categoryIds= array('41');
+                        echo 'Product Assigned to New Arrival Category';
+                        $categoryLinkRepository->assignProductToCategories($sku, $categoryIds);
+                    }
+                    else if($currentDate > $endDate && $product->getCategoryIds()==NULL ){
+                    
+                    }
+                    else if($currentDate > $endDate && $product->getCategoryIds()==$categoryID ){
+                        echo 'Expired Product Unassigned';
+                        $CategoryLinkRepository->deleteByIds($categoryID,$sku);
+                    }
+                    else{
+                        echo 'Expired Product Unassigned';
+                        $CategoryLinkRepository->deleteByIds($categoryID,$sku);
+                    }
+                }
+                else if($product->getExcludeFromNew()==1 && $product->getCategoryIds()==$categoryID ){
+                    
+                }
+                else if($product->getCategoryIds() == NULL){
+                    
+                }
+                else{
+                    echo 'Product Excluded';
                     $CategoryLinkRepository->deleteByIds($categoryID,$sku);
                 }
-                $logger->info($product->getData()); 
              }
+        }else if($configEnabled==0)
+        {
+            echo 'Auto Category has been disabled';
         }
     }
 
--- app/code/Icube/AutoCategory/Helper/Data.php
+++ app/code/Icube/AutoCategory/Helper/Data.php
@@ -6,15 +6,21 @@ use \Magento\Store\Model\ScopeInterface;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-   const XML_PATH = 'autocategory_setting/general/enabled';
+   const XML_PATH_ENABLED = 'autocategory_setting/general/enabled';
+   const XML_PATH_RANGE = 'autocategory_setting/general/rangeday';
    
    public function __construct(Context $context)
    {
 		parent::__construct($context);
    }
    
-   public function getConfigValue()
+   public function getConfigEnabled()
    {
-       return $this->scopeConfig->getValue(self::XML_PATH, ScopeInterface::SCOPE_STORE);
+       return $this->scopeConfig->getValue(self::XML_PATH_ENABLED, ScopeInterface::SCOPE_STORE);
+   }
+
+   public function getConfigRange()
+   {
+       return $this->scopeConfig->getValue(self::XML_PATH_RANGE, ScopeInterface::SCOPE_STORE);
    }
 }
